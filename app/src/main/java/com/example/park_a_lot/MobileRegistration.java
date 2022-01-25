e082c60ab216f3e6fd7d12d20798b7687b37e6e0
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-01-25 21:16:32 +0530

    OTP UI Change

--- app/src/main/java/com/example/park_a_lot/MobileRegistration.java
+++ app/src/main/java/com/example/park_a_lot/MobileRegistration.java
@@ -6,19 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MobileRegistration extends AppCompatActivity {
-    Button mVerify;
+    Button mSendOTP, mVerifyButton;
+    TextView mResendOTP;
+    EditText mOTP1,mOTP2,mOTP3,mOTP4,mOTP5,mOTP6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_registration);
-        mVerify = findViewById(R.id.Verifybutton);
-        mVerify.setOnClickListener(new View.OnClickListener() {
+        mSendOTP = findViewById(R.id.SendOTP);
+        mVerifyButton = findViewById(R.id.Verifybutton);
+        mResendOTP = findViewById(R.id.resendOtp);
+        mSendOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),VehicleRegistration.class));
+                mVerifyButton.setVisibility(View.VISIBLE);
+                mResendOTP.setVisibility(View.VISIBLE);
+                mSendOTP.setVisibility(View.INVISIBLE);
+                mOTP1.setVisibility(View.VISIBLE);
+                mOTP2.setVisibility(View.VISIBLE);
+                mOTP3.setVisibility(View.VISIBLE);
+                mOTP4.setVisibility(View.VISIBLE);
+                mOTP5.setVisibility(View.VISIBLE);
+                mOTP6.setVisibility(View.VISIBLE);
             }
         });
     }
