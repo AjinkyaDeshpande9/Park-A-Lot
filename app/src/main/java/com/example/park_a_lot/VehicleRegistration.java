08a8a4d9087a49e73d640ca0cd934e8d4ef2d723
Ajinkya_Deshpande <deshpande.ajinkya09@gmail.com>
2022-01-26 23:00:15 +0530

    OTP Verification Done

--- app/src/main/java/com/example/park_a_lot/VehicleRegistration.java
+++ app/src/main/java/com/example/park_a_lot/VehicleRegistration.java
@@ -1,5 +1,6 @@
 package com.example.park_a_lot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,34 +15,37 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class VehicleRegistration extends AppCompatActivity {
     String[] items = {"Two Wheeler","Three Wheeler","Four Wheeler"};
     AutoCompleteTextView autoCompleteTxt;
     ArrayAdapter<String> adapterItems;
-    Button mSubmitbtn;
-    EditText mName, mEmail, mPassword, mVecNumber, mMobNumber;
-    AutoCompleteTextView mVectype;
+    Button mProceed;
+    EditText mName, mEmail, mPassword, mVecNumber, mMobNumber, mVecType;
+
 
     FirebaseDatabase rootNode;
-    DatabaseReference reference;
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://parkalot-b98ef-default-rtdb.firebaseio.com/");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_registration);
+
         mName = findViewById(R.id.regName);
         mEmail = findViewById(R.id.regEmail);
         mPassword = findViewById(R.id.regPassword);
         mVecNumber = findViewById(R.id.regVecNumber);
         mMobNumber = findViewById(R.id.RegMob);
-        mVectype = findViewById(R.id.regvehicleType);
 
         autoCompleteTxt = findViewById(R.id.regvehicleType);
-        mSubmitbtn = findViewById(R.id.Submitbtn);
+        mProceed = findViewById(R.id.Submitbtn);
         adapterItems = new ArrayAdapter<String>(this,R.layout.list_items,items);
         autoCompleteTxt.setAdapter(adapterItems);
 
@@ -57,28 +61,55 @@ public class VehicleRegistration extends AppCompatActivity {
         });
 
 
-        mSubmitbtn.setOnClickListener(new View.OnClickListener() {
+        mProceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 rootNode= FirebaseDatabase.getInstance();
                 reference = rootNode.getReference("Users") ;
 
                 //Getting all the values
-                String uName = getIntent().getStringExtra("uName");
-                String uEmail = getIntent().getStringExtra("uEmail");
-                String uPassword = getIntent().getStringExtra("uPassword");
-                String uMobNumber = getIntent().getStringExtra("uMobNumber");
-                String uVecNumber = getIntent().getStringExtra("uVecNumber");
-                String uVectype = getIntent().getStringExtra("uVectype");
+                String uName = mName.getText().toString();
+                String uEmail = mEmail.getText().toString();
+                String uPassword = mPassword.getText().toString();
+                String uMobNumber = mMobNumber.getText().toString();
+                String uVecNumber = mVecNumber.getText().toString();
+                String uVectype = mVecType.getText().toString();
 
-//                UserHelperClass helperClass = new UserHelperClass(uName,uEmail,uPassword,uMobNumber,uVecNumber,uVectype);
-//                       reference.setValue(helperClass);
+                //     UserHelperClass helperClass = new UserHelperClass(uName,uEmail,uPassword,uMobNumber,uVecNumber);
 
-                startActivity( new Intent(getApplicationContext(),MainActivity.class));
-//
-//                //get values
-//                String name = mName.getEditableText().
-//
+
+                reference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        //check if phone not registered
+                        if(snapshot.hasChild(uName)){
+                            Toast.makeText(VehicleRegistration.this, "Name already registered", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            //using Name as unique identity of the user(temporary)
+                            //other details comes under the above mentioned unique ID
+                            reference.child("users").child(uName).child("Email").setValue(uEmail);
+                            reference.child("users").child(uName).child("Password").setValue(uPassword);
+                            reference.child("users").child(uName).child("Mobile Number").setValue(uMobNumber);
+                            reference.child("users").child(uName).child("Vehicle Number").setValue(uVecNumber);
+                            reference.child("users").child(uName).child("Vehicle Type").setValue(uVectype);
+
+                            //success message
+                            Toast.makeText(VehicleRegistration.this, "User registered successfully.", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
 
 
             }
