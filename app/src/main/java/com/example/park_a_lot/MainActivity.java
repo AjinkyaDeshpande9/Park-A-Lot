6fc5bfe7234f600d2a61fdb7976f24449cb6ee35
Allen Dsouza <allendsouza011@gmail.com>
2022-03-18 20:52:15 +0530

    PARKING SLOTS GUI ADDED

--- app/src/main/java/com/example/park_a_lot/MainActivity.java
+++ app/src/main/java/com/example/park_a_lot/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.park_a_lot;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +28,7 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button bottomsheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +86,65 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.framelayout, fragment);
             fragmentTransaction.commit();
+            bottomsheet = findViewById(R.id.more_options);
+            bottomsheet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    showDialog();
+
+                }
+            });
 
         }
+
+    private void showDialog() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        LinearLayout editLayout = dialog.findViewById(R.id.layoutEdit);
+        LinearLayout shareLayout = dialog.findViewById(R.id.layoutShare);
+        LinearLayout uploadLayout = dialog.findViewById(R.id.layoutUpload);
+
+        editLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Edit is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        shareLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Share is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        uploadLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this,"Upload is Clicked",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+    }
 
     }
